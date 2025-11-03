aa6f01dcd9449bded275ecd7237271e9045f3588
penexper <lorenzo.sala969@edu.unito.it>
2025-11-03 23:42:14 +0100

    patchmaker che funziona!!

--- patchmaker.py
+++ patchmaker.py
@@ -2,94 +2,112 @@ import cv2
 import numpy as np
 import os
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
 
-rng = np.random.default_rng(100)
 
-processed_path_base = "data/processed"
-bzd_path_base = "data/processed/bzd"
+processed_read_base = "data/processed"
 out_dir = "data/patches"
-bzd_patch_base = "data/patches/bzd"
-ctr_patch_base = "data/patches/ctr"
-gsc_patch_base = "data/patches/gsc"
+bzd_save_base = "data/patches/bzd"
+ctr_save_base = "data/patches/ctr"
+gsc_save_base = "data/patches/gsc"
 
-os.makedirs(bzd_patch_base, exist_ok=True)
-os.makedirs(ctr_patch_base, exist_ok=True)
-os.makedirs(gsc_patch_base, exist_ok=True)
+os.makedirs(bzd_save_base, exist_ok=True)
+os.makedirs(ctr_save_base, exist_ok=True)
+os.makedirs(gsc_save_base, exist_ok=True)
 
 PATCH_SIZE = 512       #dimensione patch (in pixel)
 N_PATCHES = 20         #quante patch estrarre
 MIN_BLACK_RATIO = 0.105 # minimo  di segnale in percentuale
 
-#ok da qui in avanti il codice è parecchio incasinato, occhio ai commenti
+#ok da qui in avanti il codice è parecchio incasinato (ma va'?), occhio ai commenti
 
-#ciclo su tutte le sottocartelle di processed/bzd, che corrispondono per costruzione alle sottocartelle di pages
-for folder in sorted(os.listdir(bzd_path_base)):
+def patchmaker(args):
 
-    #creo cartelle per gli output output
-    bzd_dir = os.path.join(out_dir,"bzd",folder)
-    gsc_dir = os.path.join(out_dir,"gsc",folder)
-    ctr_dir = os.path.join(out_dir,"ctr",folder)
-    os.makedirs(bzd_dir, exist_ok=True)
-    os.makedirs(ctr_dir, exist_ok=True)
-    os.makedirs(gsc_dir, exist_ok=True)
+    rng = np.random.default_rng(seed)
 
-    #ciclo sui file della sottocartella corrente
-    for file in tqdm(os.listdir(os.path.join(bzd_path_base, folder)), desc="manoscritto "):
+    folder, file = args
 
-        #seleziono il percorso del file corrente
-        bzd_path = os.path.join(bzd_path_base,folder,file)
+    #percorsi di lettura
+    bzd_read_file = os.path.join(processed_read_base, "bzd", folder, file)
 
-        #leggo il file
-        img = cv2.imread(bzd_path, cv2.IMREAD_UNCHANGED)
-        if img is None:
-            raise FileNotFoundError(f"casini vari, non sono riuscito a leggere {bzd_path}")
+    gsc_filename = file.replace("bzd_", "gsc_") #atroce
+    gsc_read_file = os.path.join(processed_read_base, "gsc", folder, gsc_filename)
 
-        h, w = img.shape
-        #contatori per evitare di cercare all'infinito patch valide
-        successes = 0
-        trials = 0
+    ctr_filename = file.replace("bzd_", "ctr_") #atroce
+    ctr_read_file = os.path.join(processed_read_base, "ctr", folder, ctr_filename)
 
-        while successes < N_PATCHES and trials < N_PATCHES * 100:
-            #scelgo due numeri a caso, saranno le coordinate della mia patch
-            y = rng.integers(0, h - PATCH_SIZE)
-            x = rng.integers(0, w - PATCH_SIZE)
-            patch = img[y:y+PATCH_SIZE, x:x+PATCH_SIZE]
+    #percorsi di scrittura
+    bzd_save_path = os.path.join(bzd_save_base, folder)
+    gsc_save_path = os.path.join(gsc_save_base, folder)
+    ctr_save_path = os.path.join(ctr_save_base, folder)
 
-            #se non aggiorno qui trials (prima di controllare se la patch è valida) sono meritatamente un cretino
-            trials += 1
-            
-            #scarto patch troppo bianche
-            black_ratio = (patch < 128).mean()
-            if black_ratio < MIN_BLACK_RATIO:
-                continue
+    #eventualmente fare cartelle...
+    os.makedirs(bzd_save_path, exist_ok=True)
+    os.makedirs(gsc_save_path, exist_ok=True)
+    os.makedirs(ctr_save_path, exist_ok=True)
 
-            #scrivo i file, pregando che i percorsi siano giusti
-            bzd_out = os.path.join(bzd_dir, f"patch_{successes+1:03d}.png")
-            cv2.imwrite(bzd_out, patch)
+    #caricare immagini
+    bzd_img = cv2.imread(bzd_read_file, cv2.IMREAD_UNCHANGED)
+    gsc_img = cv2.imread(gsc_read_file, cv2.IMREAD_UNCHANGED)
+    ctr_img = cv2.imread(ctr_read_file, cv2.IMREAD_UNCHANGED)
 
-            #ora devo usare la stessa patch per tirare fuoir le immagini da grayscale e countour
+    if bzd_img is None or gsc_img is None or ctr_img is None:
+        return f"casini con i file"
+    
+    #carica dimensioni e inizializza i contatori
+    h, w = bzd_img.shape
+    successes = 0
+    trials = 0
 
-            gsc_filename = file.replace("bzd_", "gsc_") #atroce
-            gsc_path = os.path.join(processed_path_base, "gsc",folder,gsc_filename)
-            gsc_img = cv2.imread(gsc_path, cv2.IMREAD_UNCHANGED)
-            if img is None:
-                raise FileNotFoundError(f"casini vari, non sono riuscito a leggere {gsc_path}")
+    #inizia a estrarre patches dall'immagine bzd
+    while successes < N_PATCHES and trials < N_PATCHES * 100:
+        
+        #aggiorna ORA il contatore di trials se non vuoi essere un cretino (lo sei comunque)
+        trials += 1
 
-            patch = gsc_img[y:y+PATCH_SIZE, x:x+PATCH_SIZE]
+        #scelgo i numeri random
+        y = rng.integers(0, h - PATCH_SIZE)
+        x = rng.integers(0, w - PATCH_SIZE)
+        patch = bzd_img[y:y+PATCH_SIZE, x:x+PATCH_SIZE]
 
-            gsc_out = os.path.join(gsc_dir, f"patch_{successes+1:03d}.png")
-            cv2.imwrite(gsc_out, patch)
+        #scarto patch troppo bianche
+        black_ratio = (patch < 128).mean()
+        if black_ratio < MIN_BLACK_RATIO:
+            continue
 
-            ctr_filename = file.replace("bzd_", "ctr_") #atroce
-            ctr_path = os.path.join(processed_path_base, "ctr",folder,ctr_filename)
-            ctr_img = cv2.imread(ctr_path, cv2.IMREAD_UNCHANGED)
-            patch = ctr_img[y:y+PATCH_SIZE, x:x+PATCH_SIZE]
+        #scrivo la patch se è buona
+        basename = os.path.splitext(file)[0] #identificatore
+        bzd_out = os.path.join(bzd_save_path, f"{basename}patch_{successes+1:03d}.png")
+        cv2.imwrite(bzd_out, patch)
 
-            ctr_out = os.path.join(ctr_dir, f"patch_{successes+1:03d}.png")
-            cv2.imwrite(ctr_out, patch)
+        #ora devo usare la stessa patch per tirare fuoir le immagini da grayscale e countour
 
-            successes += 1
+        patch = gsc_img[y:y+PATCH_SIZE, x:x+PATCH_SIZE]
+        gsc_out = os.path.join(gsc_save_path, f"{basename}patch_{successes+1:03d}.png")
+        cv2.imwrite(gsc_out, patch)
 
-        print(f"Estratte {successes} patch salvate nelle varie sottocartelle")
+        patch = ctr_img[y:y+PATCH_SIZE, x:x+PATCH_SIZE]
 
-#e ora bisogna parallelizzare per forza...
+        ctr_out = os.path.join(ctr_save_path, f"{basename}patch_{successes+1:03d}.png")
+        cv2.imwrite(ctr_out, patch)
+
+        successes += 1
+
+    return f"{folder}/{file}: {successes} patch"
+    
+
+#preparo una lista di tuple file/sottocartella così da avere tutti i task separati
+tasks = []
+for i, folder in enumerate(sorted(os.listdir(os.path.join(processed_read_base, "bzd")))):
+    folder_path = os.path.join(processed_read_base, "bzd", folder)
+    for j, file in enumerate(os.listdir(folder_path)):
+        if file.endswith(".png"):
+            seed =100 + i * 1000 + j
+            tasks.append((folder, file))
+
+#parallelizzo, che dio ce la mandi buona
+with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+    results = list(tqdm(executor.map(patchmaker, tasks),
+                        total=len(tasks), desc="Estrazione patch"))
+
+print("\n".join(results)) 
